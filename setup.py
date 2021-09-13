faa8270b6d31f4e4d6b604eef88d470530d97274
niko <vlahovic.nikolai@gmail.com>
2021-09-13 21:17:34 +0200

    Updated version: Fixed Typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="CouchDB3",
-    version="0.0.1",
+    version="1.0.0",
     author="Nikolai Vlahovic",
     author_email="nikolai@nexup.com",
     description="A wrapper around the CouchDB API.",
