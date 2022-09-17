bb282c466590a53f9b8e63127cf3be7e29a32776
Niko <vlahovic.nikolai@gmail.com>
2022-09-17 22:02:48 +0200

    Updated version patch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="CouchDB3",
-    version="1.0.6",
+    version="1.0.7",
     author="Nikolai Vlahovic",
     author_email="nikolai@nexup.com",
     description="A wrapper around the CouchDB API.",
