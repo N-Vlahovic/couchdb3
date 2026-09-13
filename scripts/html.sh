e48569eb7bade6dd1bb64066aeacd60403b98ef8
Niko <n.vlahovic@proton.me>
2026-09-13 10:40:02 +0200

    docs: regenerate API docs for v3.4.0; fix html.sh pdoc3 invocation
    - Regenerate docs with new design_docs, delete_index, has_db methods
    - html.sh: call pdoc3 (not pdoc) so --html flag is honored

--- scripts/html.sh
+++ scripts/html.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 uv pip install pdoc3 --quiet
 rm -rf docs
-uv run pdoc --html -o docs src/couchdb3 --force
+uv run pdoc3 --html -o docs src/couchdb3 --force
 mv docs/couchdb3/* docs/
 rm -rf docs/couchdb3
 
